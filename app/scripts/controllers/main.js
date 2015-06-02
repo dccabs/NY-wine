6431229b05996e6ff8acae0146e62ab74145f167
Dan Cabaniss <dccabs@gmail.com>
2015-06-02 09:28:24 -0500

    commiting all files

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nyWineApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nyWineApp
+ */
+angular.module('nyWineApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
