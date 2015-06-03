bd5711b03ddf3924d1193b8a3f2353696cdf7c40
Dan Cabaniss <dccabs@gmail.com>
2015-06-02 21:14:32 -0500

    added some click events to regions

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -21,7 +21,18 @@ angular.module('nyWineApp')
       niagraEscarpment: { position: [43.168962, -79.005821]},
       hudsonRiver: { position: [41.630632, -73.959698]}
     };
+
     $scope.getRadius = function(num) {
       return Math.sqrt(num) * 100;
+    };
+
+    $scope.mapZoom = 7;
+
+    $scope.mapCoordinates = [42.7534979, -75.8092041];
+
+    $scope.regionClick = function() {
+      console.log(this.center);
+      $scope.mapCoordinates = [this.center.A, this.center.F];
+      $scope.mapZoom = 10;
     };
   });
